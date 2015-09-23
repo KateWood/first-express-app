1819d34a120c7ed059dd649f5e910257fa30872a
Kate Wood <katewood611@gmail.com>
2015-09-22 17:03:11 -0700

    adds index, contact, and about routes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
+var router = express.Router();
 
 app.get('/', function(req, res) {
 	res.send('Hello World');
@@ -9,3 +10,17 @@ app.get('/', function(req, res) {
 app.listen(port);
 
 console.log('Server started on ' + port);
+
+router.get('/', function(req, res) {
+	res.send('index');
+})
+
+router.get('/contact', function(req, res) {
+	res.send('contact');
+})
+
+router.get('/about', function(req, res) {
+	res.send('about');
+})
+
+app.use('/', router);
