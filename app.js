61e7a70b8fa2dc5e632be1493f81bed73f6c017b
Kate Wood <katewood611@gmail.com>
2015-09-22 17:29:15 -0700

    adds cars routes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 var router = express.Router();
+var carsRouter = express.Router();
 
 app.get('/', function(req, res) {
 	res.send('Hello World');
@@ -13,14 +14,42 @@ console.log('Server started on ' + port);
 
 router.get('/', function(req, res) {
 	res.send('index');
-})
+});
 
 router.get('/contact', function(req, res) {
 	res.send('contact');
-})
+});
 
 router.get('/about', function(req, res) {
 	res.send('about');
-})
+});
+
+carsRouter.get('/', function(req, res) {
+	// INDEX
+});
+
+carsRouter.get('/:id', function(req, res) { 
+	// SHOW 
+});
+
+carsRouter.get('/new', function(req, res) { 
+	// NEW 
+});
+
+carsRouter.post('/', function(req, res) { 
+	// CREATE 
+});
+
+carsRouter.get('/:id/edit', function(req, res) { 
+// SHOW });
+
+carsRouter.put('/:id', function(req, res) { // UPDATE });
+
+carsRouter.delete('/:id', function(req, res) { // DELETE });
+
+app.use("/cars", carsRouter)
 
 app.use('/', router);
+
+
+
